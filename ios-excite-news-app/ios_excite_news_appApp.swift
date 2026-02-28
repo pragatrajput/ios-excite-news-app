eeb7be9901a0021776a899e726c48ca4ea02884e
PragatRajput <rajputpragat@gmail.com>
2026-02-28 10:45:16 +0530

    setup initial project

---
+++ ios-excite-news-app/ios_excite_news_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_excite_news_appApp.swift
+//  ios-excite-news-app
+//
+//  Created by Pragat Rajput on 28/02/26.
+//
+
+import SwiftUI
+
+@main
+struct ios_excite_news_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
